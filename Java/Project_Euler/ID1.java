10c27ab890c5cd6063d899523d475a8bfd4884ec
Marisa Poorboy <mpoorboy@users.noreply.github.com>
2015-12-15 18:03:56 -0800

    Update ID1.java
    Included answer

--- Java/Project_Euler/ID1.java
+++ Java/Project_Euler/ID1.java
@@ -1,6 +1,7 @@
 /*
 ID: 1
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of 
+these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
 */
 class ID1 {
 	public static void main(String args[]) {
@@ -14,3 +15,4 @@ class ID1 {
 	}
 
 }
+//Answer: 233168
