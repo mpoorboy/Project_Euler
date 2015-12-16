6318a66ccec14ea78d4af4672207b93415eb7c8f
Marisa Poorboy <mpoorboy@users.noreply.github.com>
2015-12-15 18:45:11 -0800

    Create ID6.java

---
+++ Java/Project_Euler/ID6.java
@@ -0,0 +1,18 @@
+/*
+ID: 6
+The sum of the squares of the first ten natural numbers is:
+1^2 + 2^2 + ... + 10^2 = 385
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)^2 = 55^2 = 3025
+Hence the difference between the sum of the squares of the 
+first ten natural numbers and the square of the sum is:
+3025 − 385 = 2640
+Find the difference between the sum of the squares of the 
+first one hundred natural numbers and the square of the sum.
+*/
+//Algorithm: [3(n^4)+2(n^3)+3(n^2)-2(n^1)]/12
+public class ID6 {
+  public static void main(String[] args) {
+  }
+}
+//Answer: 25164150
