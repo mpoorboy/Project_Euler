e379cedc97f14e5949fe7ce2ef67a4a1b0dd7e67
Marisa Poorboy <mpoorboy@users.noreply.github.com>
2016-02-06 23:44:45 -0800

    Refactored to use methods

--- Java/ID1.java
+++ Java/ID1.java
@@ -1,18 +1,20 @@
 /*
 ID: 1
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of 
-these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. 
+Find the sum of all the multiples of 3 or 5 below 1000.
 */
 class ID1 {
-	public static void main(String args[]) {
-		int n;
+	public static int SumDivisibleBy(int n) {
 		int sum = 0;
-	for (n = 0; n < 1000; n++) {
-		if ((n%3 == 0) || (n%5 == 0))
-		sum += n;
+		for (int i = 0; i <= 999; i++) {
+			int p = 999 / n;
+			sum = n * (p * (p + 1)) / 2;
 		}
-	System.out.println(sum);
+		return sum;
 	}
 
+	public static void main(String[] args) {
+		System.out.println(SumDivisibleBy(3) + SumDivisibleBy(5) - SumDivisibleBy(15));
+	}
 }
-//Answer: 233168
+// Answer: 233168
