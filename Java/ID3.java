c2917587aeec5c92b7d4b278737064a9ae8e9686
Marisa Poorboy <mpoorboy@users.noreply.github.com>
2016-02-08 17:23:49 -0800

    Refactored to use methods

--- Java/ID3.java
+++ Java/ID3.java
@@ -1,11 +1,9 @@
 /*
 ID: 3
-The prime factors of 13195 are 5, 7, 13 and 29.
-What is the largest prime factor of the number 600851475143?
+The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime factor of the number 600851475143?
 */
 class ID3 {
-	public static void main(String[] args) {
-		long n = 600851475143L;
+	public static long MaxPrimeFactorOf(long n) {
 		long max = 0L;
 		for (long i = 2L; i <= n; i++) {
 			for (long l = 1L; l <= Math.sqrt(i); l++) {
@@ -19,7 +17,11 @@ class ID3 {
 				}
 			}
 		}
-		System.out.println(max);
+		return max;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(MaxPrimeFactorOf(600851475143L));
 	}
 }
-//Answer: 6857
+// Answer: 6857
