19c4075d148acc5d4179cb6630c65dabb6a7d23f
Marisa Poorboy <mpoorboy@users.noreply.github.com>
2016-02-09 19:45:29 -0800

    Refactored. TODO: optimize isPrime()
    Redundant if's/for loop. Slow @ run time.

--- Java/ID10.java
+++ Java/ID10.java
@@ -1,8 +1,23 @@
 /*
+ID: 10
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17. Find the sum of all the primes below 2,000,000.
 */
 public class ID10 {
-    public static boolean isPrime(int n) {
+    public static void main(String[] args) {
+        System.out.println(SumOfPrimesLT(2000000L));
+    }
+
+    public static long SumOfPrimesLT(long max) {
+        long sum = 2L;
+        for (long i = 3L; i < max; i += 2L) {
+            if (isPrime(i)) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    public static boolean isPrime(long n) {
         boolean prime = true;
         if (n < 2)
             return false;
@@ -10,7 +25,7 @@ public class ID10 {
             return true;
         if (n % 2 == 0)
             return false;
-        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+        for (int i = 3; i <= (int) Math.sqrt(n); i += 2) {
             if (n % i == 0) {
                 prime = false;
                 break;
@@ -19,14 +34,5 @@ public class ID10 {
         return prime;
     }
 
-    public static void main(String[] args) {
-        long sum = 2L;
-        for (int i = 3; i < 2000000; i += 2) {
-            if (isPrime(i)) {
-                sum += i;
-            }
-        }
-        System.out.println(sum);
-    }
 }
 // Answer: 142913828922
